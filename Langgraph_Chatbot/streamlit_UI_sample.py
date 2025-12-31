68cb2d5835116496479794c397138efab61e622e
vishalwaghmare03 <vishalwaghmare0311@gmail.com>
2025-12-31 12:01:10 +0530

    Let's start Langgraph

---
+++ Langgraph_Chatbot/streamlit_UI_sample.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+with st.chat_message('user'):
+    st.text('hi')
+
+with st.chat_message('assistant'):
+    st.text('How can I help You?')
+
+user_input = st.chat_input('Type Here')
+
+if user_input:
+    with st.chat_message('user'):
+        st.text(user_input)
